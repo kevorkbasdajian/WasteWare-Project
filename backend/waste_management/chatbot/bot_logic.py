9d3a0f01865b64a76e70a85571e9256185bba61f
Kevork <kevorkbastajian01@gmail.com>
2025-12-10 00:30:10 +0200

    Changed the name of gemini model

--- backend/waste_management/chatbot/bot_logic.py
+++ backend/waste_management/chatbot/bot_logic.py
@@ -31,7 +31,7 @@ class WasteWareChatbot:
                 print(f"🔑 API Key (first 10 chars): {settings.GEMINI_API_KEY[:10]}")
                 
                 # Try to initialize the model
-                self.model = genai.GenerativeModel('gemini-pro')
+                self.model = genai.GenerativeModel('models/gemini-2.5-flashels')
                 print("✅ Gemini 1.5 Flash initialized successfully!")
             
         except Exception as e:
